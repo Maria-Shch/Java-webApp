692abbbec483253f023ccd5a3942a2382fe85c35
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-06-12 23:09:00 +0400

    added form validation

--- src/main/java/ru/shcherbatykh/controllers/PeopleController.java
+++ src/main/java/ru/shcherbatykh/controllers/PeopleController.java
@@ -12,59 +12,70 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.shcherbatykh.codesjava.Config;
-import ru.shcherbatykh.dao.PersonDAO;
 import ru.shcherbatykh.dao.PersonRepo;
 import ru.shcherbatykh.models.Person;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+
     private PersonRepo personRepo;
-    
+
     @Autowired
     public PeopleController(PersonRepo personRepo) {
         this.personRepo = personRepo;
     }
 
     @GetMapping()
-    public String index(Model model){ 
+    public String index(Model model) {
         model.addAttribute("people", personRepo.findAll());
         return "index";
     }
-    
+
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model){
+    public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personRepo.findById(id).get());
         return "show";
     }
-    
+
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person){
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "new";
     }
-    
+
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new";
+        }
         personRepo.save(person);
         return "redirect:/people";
     }
-    
+
     @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id){
+    public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personRepo.findById(id).get());
         return "edit";
     }
-    
+
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+            @PathVariable("id") int id) {
+
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
         personRepo.save(person);
         return "redirect:/people";
     }
-    
+
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id){
+    public String delete(@PathVariable("id") int id) {
         personRepo.deleteById(id);
         return "redirect:/people";
     }
-    
+
 }
--- src/main/java/ru/shcherbatykh/models/Person.java
+++ src/main/java/ru/shcherbatykh/models/Person.java
@@ -1,25 +1,41 @@
 package ru.shcherbatykh.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 
 public class Person {
     @Id
     public Integer id;
+    
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     public String name;
+    
+     @Min(value = 0, message = "Age should be greater than 0")
+    public int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    public String email;
 
     public Person() {
     }
-    
-    public Person(int id, String name) {
+
+    public Person(Integer id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -30,5 +46,23 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
     
 }
